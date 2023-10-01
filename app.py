933f7ac13c93a5c1ec5e7d251fa47a2594143542
Theara Ya <theara.ya@outlook.com>
2023-10-01 16:32:47 -0400

    add routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db
 
@@ -13,3 +13,39 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 connect_db(app)
 
 toolbar = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def get_root():
+    """ Redirect to /register. """
+    return redirect('/')
+
+
+@app.route('/register')
+def get_register():
+    """ Get registration form. """
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['POST'])
+def post_register():
+    """ Process registration and redirect to /secret on success. """
+    return redirect('/')
+
+
+@app.route('/login')
+def get_login():
+    """ Get login form. """
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def post_login():
+    """ Process login and redirect to /secret on success. """
+    return redirect('/')
+
+
+@app.route('/secret')
+def get_secret():
+    """ Allows only authenticated users. """
+    return render_template('secret.html')
